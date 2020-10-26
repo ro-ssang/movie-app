eafc05a40d5b0c6772865b3af45373d56b37fc4a
rossa <fashionfish@naver.com>
2020-10-26 18:48:44 +0900

    Updates

---
+++ precache-manifest.804827f51ac400e6a303d489b5d6a0af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0104e74b5400646ca16ab9b80ebe384c",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "0de066ee097448a8caf6",
+    "url": "/movie-app/static/css/main.79c0f19d.chunk.css"
+  },
+  {
+    "revision": "39f75bacc7e29a4ac856",
+    "url": "/movie-app/static/js/2.e533ec29.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/movie-app/static/js/2.e533ec29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0de066ee097448a8caf6",
+    "url": "/movie-app/static/js/main.6485c198.chunk.js"
+  },
+  {
+    "revision": "daeaf96d14270f7d7af2",
+    "url": "/movie-app/static/js/runtime-main.2be5c0aa.js"
+  }
+]);
